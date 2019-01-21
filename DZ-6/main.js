bb7a53a70b6a15ec16d6c6c6b9aa5dbd415fd249
Edsergeev2014 <ed.sergeev2014@yandex.ru>
2019-01-21 12:54:31 +0300

    Здравствуйте Игорь!
    Направляю домашнюю работу.
    Сделаны п.1,2,3.
    4-й сделаю позже.
    
    Подробнее по пунктам:
    п.1а - все сделано и все хорошо работает.
    п.1б - эффекты срабатывают, например Tip (вывод Title-подсказки). А вот с Bounce - возникли трудности, так как мне не всегда удается улавливать событие. С этим у меня какие-то сложности и я не понимаю в чем. Много эсперементировал, но считыватель события то срабатывает, то нет. Я повторю занятия по этой теме и поработаю еще с материалами, чтобы решить с этим вопросом и взять его под контроль.
    
    п.2 Вывод ошибок с помощью виджита Dialog. Виджет, конечно я сделал, чтобы появлялся, но здесь та же картина. У меня также часто происходит неконтроль срабатывания события.
    
    п.3 Карусель (слайдер) сделан на основе предоставленных сайтов, это не сложно. Но вот с настройками еще надо поработать. Например, неполучается отобразить боковые кнопки пролистывания.
    
    Для проверки ДЗ файлы:
    п.1 и 2 - form-validate-jquery-ui.html
    п.3 - slider.html в папке 'slick-1.8.1'.
    
    Спасибо!

---
+++ DZ-6/main.js
@@ -0,0 +1,11 @@
+     //Обработка событий
+      //$( ".input:invalid" ).on( "click", function() {
+        //$(this).effect( "bounce", "slow" );
+        //console.log($(this));
+      //});
+
+      //$(document).ready(function(){
+      //$('.your-class').slick({
+      //  setting-name: setting-value
+      //});
+    //});
